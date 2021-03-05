3d32c2b445cc06670347f2b8b6b9208bd75bff56
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-03-05 16:15:48 -0500

    Now only scrape episodes whose agents ELOs are all above a minimum threshold

--- replays_to_train_data.py
+++ replays_to_train_data.py
@@ -4,6 +4,7 @@ import json
 import kaggle_environments
 import numpy as np
 from pathlib import Path
+import pandas as pd
 from scipy import stats
 import tqdm
 from typing import *
@@ -75,6 +76,30 @@ def batch_split_replay_files(replay_paths: List[Path], save_dir: Path, force: bo
     with open(save_dir / 'all_saved_episodes.txt', 'w') as f:
         f.writelines([f'{rn}\n' for rn in saved_replay_names])
     print(f'Successfully saved {file_counter:,} steps from {len(replay_paths)} replays.')
+    print(f'{len(saved_replay_names)} out of {len(all_replay_names)} replays saved in total.')
+
+
+def select_episodes(replay_paths: List[Path], metadata_path: Path, threshold: float) -> List[Path]:
+    # Load Episodes and EpisodeAgents, and filter for hungry-geese competition
+    episodes_df = pd.read_csv(metadata_path / 'Episodes.csv')
+    epagents_df = pd.read_csv(metadata_path / 'EpisodeAgents.csv')
+    episodes_df = episodes_df[episodes_df.CompetitionId == 25401]
+    epagents_df = epagents_df[epagents_df.EpisodeId.isin(episodes_df.Id)]
+
+    epagents_df.fillna(0, inplace=True)
+    epagents_df = epagents_df.sort_values(by=['Id'], ascending=False)
+
+    latest_scores_df = epagents_df.loc[epagents_df.groupby('SubmissionId').EpisodeId.idxmax(), :].sort_values(
+        by=['UpdatedScore'])
+    latest_scores_df['LatestScore'] = latest_scores_df.UpdatedScore
+    latest_scores_df = latest_scores_df[['SubmissionId', 'LatestScore']]
+    epagents_df = epagents_df.merge(latest_scores_df, left_on='SubmissionId', right_on='SubmissionId',
+                                    how='outer').sort_values(by=['LatestScore'])
+
+    episode_min_scores = epagents_df.groupby('EpisodeId').LatestScore.min()
+    ep_to_score = episode_min_scores[episode_min_scores >= threshold].to_dict()
+
+    return [rp for rp in replay_paths if int(rp.stem) in ep_to_score.keys()]
 
 
 if __name__ == '__main__':
@@ -98,5 +123,24 @@ if __name__ == '__main__':
         action='store_true',
         help='Including this option will overwrite existing folders'
     )
+    parser.add_argument(
+        '-t',
+        '--threshold',
+        type=float,
+        default=None,
+        help='The minimum ELO threshold of agents to include an episode. Leave empty to process all episodes'
+    )
+    parser.add_argument(
+        '-m',
+        '--metadata_path',
+        type=Path,
+        default=Path('episode_scraping/metadata'),
+        help='The path to directory containing the EpisodeAgents and Episodes .csv files. '
+             'Default: episode_scraping/metadata'
+    )
     args = parser.parse_args()
-    batch_split_replay_files(**vars(args))
+    if args.threshold is not None:
+        selected_replay_paths = select_episodes(args.replay_paths, args.metadata_path, args.threshold)
+    else:
+        selected_replay_paths = args.replay_paths
+    batch_split_replay_files(selected_replay_paths, args.save_dir, args.force)
