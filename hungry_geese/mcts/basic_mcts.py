Fixed type-casting bug in alphagoose, and working on adding action-masking to loss function

hungry_geese/mcts/basic_mcts.py
@@ -20,13 +20,17 @@ class Node:
         if available_actions_masks is None:
             self.available_actions_masks = np.ones_like(initial_policies)
         else:
-            self.available_actions_masks = available_actions_masks
+            self.available_actions_masks = np.where(
+                available_actions_masks.any(axis=-1, keepdims=True),
+                available_actions_masks,
+                np.ones_like(available_actions_masks)
+            )
         self.initial_policies = initial_policies
         assert self.initial_policies.shape == (self.n_geese, 4)
         assert np.all(0. <= self.initial_policies) and np.all(self.initial_policies <= 1.)
         assert np.allclose(self.initial_policies.sum(axis=-1), 1.)
         # Re-normalize policy distribution
-        self.initial_policies = self.initial_policies * available_actions_masks
+        self.initial_policies = self.initial_policies * self.available_actions_masks
         if np.any(self.initial_policies.sum(axis=1) == 0.):
             if np.logical_and(self.initial_policies.sum(axis=1) == 0.,
                               self.available_actions_masks.any(axis=1)).any():

hungry_geese/training/alphagoose/alphagoose_data.py
@@ -2,6 +2,7 @@ from kaggle_environments.envs.hungry_geese.hungry_geese import Action
 import numpy as np
 from pathlib import Path
 import random
+import time
 import torch
 from torch.utils.data import Dataset
 from typing import *
@@ -24,7 +25,7 @@ class AlphaGooseDataset(Dataset):
             raise ValueError('Other obs_types have not yet been implemented, '
                              'they will need different data concatenation')
 
-    def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         step = None
         for i in range(10):
             # Try and reread the file up to 10 times if an error occurs
@@ -39,6 +40,7 @@ class AlphaGooseDataset(Dataset):
 
         state = create_obs_tensor(step, self.obs_type)
         policies = []
+        available_actions_masks = []
         final_ranks = []
         head_locs = []
         still_alive = []
@@ -47,17 +49,20 @@ class AlphaGooseDataset(Dataset):
             final_ranks.append(agent['final_rank'])
             if still_alive[-1]:
                 policies.append(np.array(agent['policy']))
+                available_actions_masks.append(np.array(agent['available_actions_mask']))
                 head_locs.append(step[0]['observation']['geese'][agent_idx][0])
             else:
                 policies.append(np.zeros(4))
+                available_actions_masks.append(np.zeros(4))
                 head_locs.append(-1)
         ranks_rescaled = 2. * np.array(final_ranks) / (4. - 1.) - 1.
 
-        sample = (state.squeeze(axis=0),
-                  np.stack(policies, axis=0),
-                  ranks_rescaled,
-                  np.array(head_locs),
-                  np.array(still_alive))
+        sample = (state.squeeze(axis=0).astype(np.float32),
+                  np.stack(policies, axis=0).astype(np.float32),
+                  np.stack(available_actions_masks, axis=0).astype(np.bool),
+                  ranks_rescaled.astype(np.float32),
+                  np.array(head_locs).astype(np.int64),
+                  np.array(still_alive).astype(np.bool))
 
         if self.transform is not None:
             sample = self.transform(sample)
@@ -104,11 +109,11 @@ class AlphaGoosePretrainDataset(Dataset):
                 head_locs.append(-1)
         ranks_rescaled = 2. * np.array(final_ranks) / (4. - 1.) - 1.
 
-        sample = (state.squeeze(axis=0),
-                  np.array(actions),
-                  ranks_rescaled,
-                  np.array(head_locs),
-                  np.array(still_alive))
+        sample = (state.squeeze(axis=0).astype(np.float32),
+                  np.array(actions).astype(np.int64),
+                  ranks_rescaled.astype(np.float32),
+                  np.array(head_locs).astype(np.int64),
+                  np.array(still_alive).astype(np.bool))
 
         if self.transform is not None:
             sample = self.transform(sample)
@@ -132,19 +137,21 @@ class AlphaGooseRandomReflect:
     def __call__(
             self,
             sample: Sequence[np.ndarray]
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-        state, policies, ranks_rescaled, head_locs, still_alive = sample
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        state, policies, available_actions_masks, ranks_rescaled, head_locs, still_alive = sample
         if self.obs_type == ObsType.COMBINED_GRADIENT_OBS:
             new_head_locs = np.arange(state.shape[-2] * state.shape[-1]).reshape(*state.shape[-2:])
             # Flip vertically
             if random.random() < 0.5:
                 state = np.flip(state, axis=-2)
                 policies = flip_policies(policies, 'rows')
+                available_actions_masks = flip_policies(available_actions_masks, 'rows')
                 new_head_locs = np.flip(new_head_locs, axis=-2)
             # Flip horizontally
             if random.random() < 0.5:
                 state = np.flip(state, axis=-1)
                 policies = flip_policies(policies, 'cols')
+                available_actions_masks = flip_policies(available_actions_masks, 'cols')
                 new_head_locs = np.flip(new_head_locs, axis=-1)
             head_locs = np.where(
                 still_alive,
@@ -153,7 +160,7 @@ class AlphaGooseRandomReflect:
             )
         else:
             raise ValueError(f'Not yet a supported obs_type: {self.obs_type}')
-        return state, policies, ranks_rescaled, head_locs, still_alive
+        return state, policies, available_actions_masks, ranks_rescaled, head_locs, still_alive
 
 
 class PretrainRandomReflect:
@@ -178,12 +185,12 @@ class PretrainRandomReflect:
             if random.random() < 0.5:
                 state = np.flip(state, axis=-2)
                 actions = flip_actions(actions, 'rows')
-                new_head_locs = np.flip(new_head_locs, axis=-2, )
+                new_head_locs = np.flip(new_head_locs, axis=-2)
             # Flip horizontally
             if random.random() < 0.5:
-                state = np.flip(state, axis=-1, )
+                state = np.flip(state, axis=-1)
                 actions = flip_actions(actions, 'cols')
-                new_head_locs = np.flip(new_head_locs, axis=-1, )
+                new_head_locs = np.flip(new_head_locs, axis=-1)
             head_locs = np.where(
                 still_alive,
                 new_head_locs.ravel()[head_locs.ravel()].reshape(head_locs.shape),
@@ -196,19 +203,14 @@ class PretrainRandomReflect:
 
 class ToTensor:
     """
-    Given a tuple of (state, actions, ranks_rescaled, head_locs, still_alive) arrays, convert them to pytorch tensors
+    Given a sequence of numpy arrays, convert them to pytorch tensors
     """
 
     def __call__(
             self,
-            sample: Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        state, actions, ranks_rescaled, head_locs, still_alive = sample
-        return (torch.tensor(state.copy(), dtype=torch.float32),
-                torch.tensor(actions, dtype=torch.int64),
-                torch.tensor(ranks_rescaled, dtype=torch.float32),
-                torch.tensor(head_locs, dtype=torch.int64),
-                torch.tensor(still_alive, dtype=torch.bool))
+            sample: Sequence[np.ndarray]
+    ) -> List[torch.Tensor]:
+        return [torch.from_numpy(arr.copy()) for arr in sample]
 
 
 def flip_policies(policies: np.ndarray, flipped_rows_or_cols: str) -> np.ndarray:

hungry_geese/training/alphagoose/alphagoose_data_generator.py
@@ -42,6 +42,7 @@ def alphagoose_data_generator_worker(
         terminal_value_func=terminal_value_func,
         **mcts_kwargs
     ) for _ in range(n_envs_per_worker)]
+    available_actions_masks = [[] for _ in range(n_envs_per_worker)]
     pre_search_policies = [[] for _ in range(n_envs_per_worker)]
     post_search_policies = [[] for _ in range(n_envs_per_worker)]
     # Create model and load weights
@@ -60,6 +61,7 @@ def alphagoose_data_generator_worker(
                     save_start_time = time.time()
                     n_saved_steps = save_episode_steps(
                         env,
+                        available_actions_masks[env_idx],
                         pre_search_policies[env_idx],
                         post_search_policies[env_idx],
                         worker_data_dir,
@@ -68,6 +70,7 @@ def alphagoose_data_generator_worker(
                     )
                     print(f'{worker_id}: Saved train examples in {time.time() - save_start_time:.2} seconds')
                     env.reset()
+                    available_actions_masks[env_idx] = []
                     pre_search_policies[env_idx] = []
                     post_search_policies[env_idx] = []
             step_start_time = time.time()
@@ -96,10 +99,12 @@ def alphagoose_data_generator_worker(
                             value_est=values_batch[idx],
                             **backprop_kwargs
                         )
-            for idx, (env, search_tree, pre_policy_list, post_policy_list) in enumerate(zip(
-                    envs, search_trees, pre_search_policies, post_search_policies
+            for idx, (env, search_tree, available_actions_list, pre_policy_list, post_policy_list) in enumerate(zip(
+                    envs, search_trees, available_actions_masks, pre_search_policies, post_search_policies
             )):
                 root_node = search_tree.get_root_node(env)
+                # Booleans are not JSON serializable
+                available_actions_list.append(root_node.available_actions_masks.astype(np.float))
                 pre_policy_list.append(root_node.initial_policies)
                 post_policy_list.append(root_node.get_improved_policies(temp=1.))
                 actions = root_node.get_improved_actions(temp=0.)
@@ -118,6 +123,7 @@ def alphagoose_data_generator_worker(
 
 def save_episode_steps(
         env: LightweightEnv,
+        available_actions_masks: List[np.ndarray],
         pre_search_policies: List[np.ndarray],
         post_search_policies: List[np.ndarray],
         worker_data_dir: Path,
@@ -131,6 +137,7 @@ def save_episode_steps(
         for agent_idx, agent in enumerate(step):
             agent['final_rank'] = agent_rankings[agent_idx]
             if agent['status'] == 'ACTIVE':
+                agent['available_actions_mask'] = list(available_actions_masks[step_idx][agent_idx])
                 agent['initial_policy'] = list(pre_search_policies[step_idx][agent_idx])
                 agent['policy'] = list(post_search_policies[step_idx][agent_idx])
         with open(worker_data_dir / f'{int(n_saved_steps)}.json', 'w') as f:

hungry_geese/training/alphagoose/alphagoose_trainer.py
@@ -83,6 +83,7 @@ class AlphaGooseTrainer:
             self.rendering_env_kwargs = None
 
         self.epoch_counter = 0
+        self.batch_counter = 0
         existing_tf_events = list(self.exp_folder.glob('*.tfevents.*'))
         for tf_event in existing_tf_events:
             os.remove(tf_event)
@@ -150,6 +151,7 @@ class AlphaGooseTrainer:
                 train_metrics['policy_loss'].append(policy_loss.detach().cpu().item())
                 train_metrics['value_loss'].append(value_loss.detach().cpu().item())
                 train_metrics['combined_loss'].append(combined_loss.detach().cpu().item())
+                self.batch_counter += 1
             self.log_train(train_metrics, len(dataloader))
 
             if self.epoch_counter % self.checkpoint_freq == 0 and self.epoch_counter > 0:
@@ -172,22 +174,45 @@ class AlphaGooseTrainer:
 
     def compute_losses(
             self,
-            state: torch.Tensor,
-            search_policy: torch.Tensor,
-            result: torch.Tensor,
-            head_locs: torch.Tensor,
-            still_alive: torch.Tensor
+            state: torch.FloatTensor,
+            search_policy: torch.FloatTensor,
+            available_actions_masks: torch.BoolTensor,
+            result: torch.FloatTensor,
+            head_locs: torch.LongTensor,
+            still_alive: torch.BoolTensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         with amp.autocast(enabled=self.use_mixed_precision):
             logits, value = self.model(state, head_locs, still_alive)
 
-            logits_masked = logits.view(-1, 4)[still_alive.view(-1, 1).expand(-1, 4)].view(-1, 4)
-            policy_masked = search_policy.view(-1, 4)[still_alive.view(-1, 1).expand(-1, 4)].view(-1, 4)
-            policy_loss = -torch.sum(policy_masked * F.log_softmax(logits_masked, dim=-1), dim=-1).mean()
+            still_alive_expanded = still_alive.view(-1, 1).expand(-1, 4)
+            logits = logits.view(-1, 4)[still_alive_expanded].view(-1, 4)
+            search_policy = search_policy.view(-1, 4)[still_alive_expanded].view(-1, 4)
+            """
+            available_actions_masks = available_actions_masks.view(-1, 4)[still_alive_expanded].view(-1, 4)
+            # Mask and renormalize policy
+            probs_masked = F.softmax(logits, dim=-1) * available_actions_masks
+            if torch.any(probs_masked.sum(dim=-1) == 0):
+                print(f'WARNING: All available actions have 0% probability for '
+                      f'{torch.sum(probs_masked.sum(dim=-1) == 0)}/{probs_masked.shape[0]} rows')
+            probs_masked = torch.where(
+                probs_masked.sum(dim=-1, keepdim=True) > 0,
+                probs_masked,
+                torch.ones_like(probs_masked)
+            )
+            probs_masked = probs_masked / probs_masked.sum(dim=-1, keepdim=True)
+            policy_loss = -torch.sum(torch.where(
+                available_actions_masks,
+                search_policy * torch.log(probs_masked),
+                torch.zeros_like(search_policy)
+            ), dim=-1).mean()
+            """
+            policy_loss = -torch.sum(search_policy * F.log_softmax(logits, dim=-1), dim=-1).mean()
+            if policy_loss.cpu().item() > 1.:
+                print(logits, search_policy, F.log_softmax(logits, dim=-1))
 
-            value_masked = value.view(-1)[still_alive.view(-1)]
-            result_masked = result.view(-1)[still_alive.view(-1)]
-            value_loss = F.mse_loss(value_masked, result_masked)
+            value = value.view(-1)[still_alive.view(-1)]
+            result = result.view(-1)[still_alive.view(-1)]
+            value_loss = F.mse_loss(value, result)
 
             combined_loss = (self.policy_weight * policy_loss +
                              self.value_weight * value_loss)
@@ -202,7 +227,7 @@ class AlphaGooseTrainer:
             for i, m in enumerate(metric):
                 self.summary_writer.add_scalar(f'Batch/train_{metric_name}',
                                                m,
-                                               (self.epoch_counter + 1) * n_batches - (n_batches - i))
+                                               self.batch_counter - (n_batches - i))
         last_lr = self.lr_scheduler.get_last_lr()
         assert len(last_lr) == 1, 'Logging per-parameter LR still needs support'
         self.summary_writer.add_scalar(f'Epoch/learning_rate',