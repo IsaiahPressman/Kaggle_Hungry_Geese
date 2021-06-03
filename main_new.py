4533b32459c440ccfe3a2520fcf3397a813e35b9
IsaiahPressman <isaiahpressman16@gmail.com>
2021-06-02 21:16:08 -0400

    Working on learnable noise weights

--- main_new.py
+++ main_new.py
@@ -12,7 +12,7 @@ from typing import *
 # See https://www.kaggle.com/c/google-football/discussion/191257
 sys.path.append('/kaggle_simulations/agent/')
 from hungry_geese.config import *
-from hungry_geese.utils import ActionMasking, row_col, print_array_one_line
+from hungry_geese.utils import STATE_TYPE, ActionMasking, row_col, print_array_one_line
 from hungry_geese.env import goose_env as ge
 from hungry_geese.env.lightweight_env import LightweightEnv, make_from_state
 from hungry_geese.mcts.basic_mcts import BasicMCTS, Node
@@ -34,10 +34,20 @@ def get_direction(from_loc: np.ndarray, to_loc: np.ndarray) -> Action:
     return DIRECTIONS_DICT[tuple(wrap(to_loc - from_loc))]
 
 
-def terminal_value_func(state: List[Dict]) -> np.ndarray:
+def terminal_value_func(state: STATE_TYPE) -> np.ndarray:
     agent_rankings = stats.rankdata([agent['reward'] for agent in state], method='average') - 1.
     ranks_rescaled = 2. * agent_rankings / (len(state) - 1.) - 1.
     return ranks_rescaled
+
+
+# Credit for scaled and inverse_scaled sigmoid:
+# https://stackoverflow.com/questions/52972158/sigmoid-scale-and-inverse-with-custom-range
+def scaled_sigmoid(x: torch.Tensor, min_val: float, max_val: float) -> torch.Tensor:
+    return min_val + (max_val - min_val) / (1. + torch.exp(-x))
+
+
+def inverse_scaled_sigmoid(x: torch.Tensor, min_val: float, max_val: float) -> torch.Tensor:
+    return torch.log((x - min_val) / (max_val - x))
 
 
 def entropy(pk: torch.Tensor, dim: int = 0, keepdim: bool = False) -> torch.Tensor:
@@ -51,13 +61,18 @@ def entropy(pk: torch.Tensor, dim: int = 0, keepdim: bool = False) -> torch.Tens
 
 
 def kl_divergence(pk: torch.Tensor, qk: torch.Tensor, dim: int = 0, keepdim: bool = False) -> torch.Tensor:
+    eps = 1e-10
+    pk = pk + eps
+    qk = qk + eps
     pk = pk / pk.sum(dim=dim, keepdim=True)
     qk = qk / qk.sum(dim=dim, keepdim=True)
+    """
     result = torch.where(
         pk != 0.,
         pk * torch.log(pk / qk),
         torch.zeros_like(pk)
-    )
+    )"""
+    result = pk * torch.log(pk / qk)
     return result.sum(dim=dim, keepdim=keepdim)
 
 
@@ -78,6 +93,11 @@ RESET_SEARCH = True
 OVERAGE_BUFFER = 1.
 PER_ROUND_BATCHED_TIME_ALLOCATION = 0.9
 BATCH_SIZE = 1
+
+STARTING_NOISE = 0.05
+MIN_NOISE = 0.02
+MAX_NOISE = 0.75
+NOISE_OPT_LR = 5.
 
 assert C_PUCT >= 0.
 
@@ -143,6 +163,7 @@ class Agent:
         self.obs_type = obs_type
 
         """
+        # Torchscript trace the model
         dummy_state = torch.zeros(self.obs_type.get_obs_spec()[1:])
         dummy_head_loc = torch.arange(4).to(dtype=torch.int64)
         still_alive = torch.ones(4).to(dtype=torch.bool)
@@ -164,11 +185,18 @@ class Agent:
         )
         self.last_head_locs = [row_col(goose[0]) for goose in obs.geese]
         self.last_actions = [Action.NORTH for _ in range(self.n_geese)]
-        self.all_actions = torch.zeros(conf.episode_steps - 1, self.n_geese, 4, dtype=torch.float32)
+        self.noise_weight_logits = inverse_scaled_sigmoid(
+            torch.tensor([STARTING_NOISE] * self.n_geese),
+            MIN_NOISE,
+            MAX_NOISE
+        )
+        self.noise_weight_logits = self.noise_weight_logits.view(1, self.n_geese, 1).requires_grad_()
+        self.noise_opt = torch.optim.SGD((self.noise_weight_logits,), lr=NOISE_OPT_LR, momentum=0.9)
+        self.all_actions = torch.zeros((conf.episode_steps - 1, self.n_geese, 4), dtype=torch.float32)
         self.all_policy_preds = torch.zeros_like(self.all_actions)
         self.all_action_masks = torch.zeros_like(self.all_actions)
 
-    def preprocess(self, obs: Observation, conf: Configuration):
+    def preprocess(self, obs: Observation, conf: Configuration) -> NoReturn:
         for goose_idx, goose in enumerate(obs.geese):
             if len(goose) > 0:
                 if self.last_head_locs[goose_idx] is not None and obs.step > 0:
@@ -205,25 +233,13 @@ class Agent:
                 list(range(self.n_geese)),
                 [a.value-1 for a in self.last_actions]
             ] = 1.
-            eps = 1e-10
-            overall_kl_divs = kl_divergence(
-                self.all_actions[:obs.step],
-                self.all_policy_preds[:obs.step],
-                dim=-1,
-                keepdim=True
-            ) / (entropy(self.all_action_masks[:obs.step], dim=-1, keepdim=True) + eps)
-            overall_kl_divs = overall_kl_divs.mean(dim=0)
-            recent_kl_divs = kl_divergence(
-                self.all_actions[obs.step-1],
-                self.all_policy_preds[obs.step-1],
-                dim=-1,
-                keepdim=True
-            ) / (entropy(self.all_action_masks[obs.step-1], dim=-1, keepdim=True) + eps)
-            print(f'Overall KL-divergences: {print_array_one_line(overall_kl_divs.numpy())}', end=' ')
-            print(f'Last step KL-divergences: {print_array_one_line(recent_kl_divs.numpy())}', end=' ')
+            update_mask = root_node.geese_still_playing.copy()
+            update_mask[self.index] = False
+            self.update_noise_weights(obs.step, update_mask)
         
         # Greedily select best action
         selected_action = tuple(Action)[my_best_action_idx].name
+        """
         print(
             f'Step: {obs.step + 1} '
             f'Index: {self.index} '
@@ -237,7 +253,7 @@ class Agent:
             f'All initial values: {print_array_one_line(root_node.initial_values)} '
             f'All policies: {print_array_one_line(final_policies)} '
             f'All Q-values: {print_array_one_line(q_vals)} '
-        )
+        )"""
         return selected_action
 
     def run_search(self, obs: Observation, env: LightweightEnv) -> Tuple[int, Node]:
@@ -315,7 +331,51 @@ class Agent:
 
         return my_best_action_idx, root_node
 
-    def batch_actor_critic_func(self, state_batch):
+    def update_noise_weights(self, current_step: int, update_mask: Sequence[bool], eps: float = 1e-10) -> NoReturn:
+        masked_actions = self.all_actions[:, update_mask]
+        masked_policy_preds = self.all_policy_preds[:, update_mask]
+        masked_action_masks = self.all_action_masks[:, update_mask]
+        masked_noise_weights = self.noise_weights[:, update_mask]
+        overall_kl_divs = kl_divergence(
+            self.all_actions[:current_step],
+            self.all_policy_preds[:current_step],
+            dim=-1,
+            keepdim=True
+        ) / (entropy(self.all_action_masks[:current_step], dim=-1, keepdim=True) + eps)
+        overall_kl_divs = overall_kl_divs.mean(dim=0)
+        print(f'Overall KL-divergences: {print_array_one_line(overall_kl_divs.numpy().ravel())} ', end='')
+
+        noise = torch.ones((1, 1, 4), dtype=torch.float32) * self.all_action_masks[:current_step]
+        noise = noise / noise.sum(dim=-1, keepdim=True)
+        masked_noise = noise[:, update_mask]
+        self.noise_opt.zero_grad()
+        kl_divs_with_noise = kl_divergence(
+            masked_actions[:current_step],
+            masked_policy_preds[:current_step] * (1. - masked_noise_weights) + masked_noise * masked_noise_weights,
+            dim=-1,
+            keepdim=True
+        ) / (entropy(masked_action_masks[:current_step], dim=-1, keepdim=True) + eps)
+        kl_divs_with_noise.mean().backward()
+        printable_noise_grads = np.where(
+            update_mask,
+            self.noise_weight_logits.grad.numpy().ravel(),
+            float('nan')
+        )
+        print(f'Noise gradients: {print_array_one_line(printable_noise_grads)} ',
+              end='')
+        self.noise_opt.step()
+
+        with torch.no_grad():
+            kl_divs_with_noise = kl_divergence(
+                self.all_actions[:current_step],
+                self.all_policy_preds[:current_step] * (1. - self.noise_weights) + noise * self.noise_weights,
+                dim=-1,
+                keepdim=True
+            ) / (entropy(self.all_action_masks[:current_step], dim=-1, keepdim=True) + eps)
+            print(f'Noise weights: {print_array_one_line(self.noise_weights.view(-1).numpy())} '
+                  f'Post-noise KL-divergences: {print_array_one_line(kl_divs_with_noise.mean(dim=0).numpy().ravel())} ')
+
+    def batch_actor_critic_func(self, state_batch: List[STATE_TYPE]) -> Tuple[np.ndarray, np.ndarray]:
         obs_list = []
         head_locs_list = []
         still_alive_list = []
@@ -355,6 +415,14 @@ class Agent:
         # Logits should be of shape (n_envs, n_geese, 4)
         # Values should be of shape (n_envs, n_geese, 1)
         return probs, np.expand_dims(final_values, axis=-1)
+
+    @property
+    def noise_weights(self) -> torch.Tensor:
+        return torch.where(
+            (torch.arange(self.n_geese) != self.index).view(self.noise_weight_logits.shape),
+            scaled_sigmoid(self.noise_weight_logits, MIN_NOISE, MAX_NOISE),
+            torch.zeros_like(self.noise_weight_logits)
+        )
 
 
 AGENT = None
